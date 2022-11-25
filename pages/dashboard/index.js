5f407ecd01b29fddcf10ab221d67b774dfdf0c28
KobiCliff <kobi.cliff05@gmail.com>
2022-11-25 12:42:22 +0100

    updated the index.js file under dashboard

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import { withAuthSync } from '../../libs/withAuthSync'
 import Footer from './../../components/Footer';
+import Header from '../../components/Header';
+import NavHeader from '../../components/NavHeader';
+import ContentBody from '../../components/ContentBody';
+import Deznav from './../../components/Deznav';
 
 
 const Dashbaord = () => {
     return (
         <>
-        <Footer />
+        {/* <div id="main-wrapper"> */}
+            <NavHeader />
+            <Header />
+            <Deznav />
+            <ContentBody />
+            <Footer />
+        {/* </div> */}
         </>
     )
 }
